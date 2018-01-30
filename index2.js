a92009f116ad02cba80364eecae9eaad734fba28
Nicolau Brasil <nicolaubrasil@gmail.com>
2018-01-29 22:53:00 -0300

    Fix identation

--- index2.js
+++ index2.js
@@ -9,10 +9,8 @@ let ALG = process.argv[3]
 let word = process.argv[2]
     
 if(ALG == 'MD5'){
-  console.log(word + ': ' +CryptoJS.MD5(word)
-  																.toString(CryptoJS.enc.Base64))
+  console.log(word + ': ' +CryptoJS.MD5(word).toString(CryptoJS.enc.Base64))
   return
 }
  
-console.log(word + ': ' +CryptoJS.SHA256(word)
-																 .toString(CryptoJS.enc.Base64))
+console.log(word + ': ' +CryptoJS.SHA256(word).toString(CryptoJS.enc.Base64))

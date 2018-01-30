74e7068db60a00d7e00e78fe58b439145134fec5
Nicolau Brasil <nicolaubrasil@gmail.com>
2018-01-29 22:50:39 -0300

    Exercise 2

---
+++ index2.js
@@ -0,0 +1,18 @@
+const CryptoJS = require("crypto-js")
+
+if(process.argv.length < 3){
+  console.log('usage: node index2.js string alg[optional]')
+  return
+}
+
+let ALG = process.argv[3]
+let word = process.argv[2]
+    
+if(ALG == 'MD5'){
+  console.log(word + ': ' +CryptoJS.MD5(word)
+  																.toString(CryptoJS.enc.Base64))
+  return
+}
+ 
+console.log(word + ': ' +CryptoJS.SHA256(word)
+																 .toString(CryptoJS.enc.Base64))
